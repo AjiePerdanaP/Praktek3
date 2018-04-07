bb8e0dadaccc40912ff8e89cd99eeda087127b99
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-07 12:59:08 +0700

    buat getter and setter

--- Praktek3/src/praktek3/Motor3.java
+++ Praktek3/src/praktek3/Motor3.java
@@ -13,4 +13,29 @@ public class Motor3 {
                 "harga \t: "+harga);
     
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public Double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+       
 }
