72d06936a46a41b60487c9712239c144ffabe795
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-07 13:04:05 +0700

    menggunakan method set

--- Praktek3/src/praktek3/MotorAksi3.java
+++ Praktek3/src/praktek3/MotorAksi3.java
@@ -3,9 +3,9 @@ package praktek3;
 public class MotorAksi3 {
     public static void main(String[] args) {
         Motor3 m = new Motor3();
-        m.merk="Baja";
-        m.warna="hitam";
-        m.harga=200.0;
+        m.setMerk("Baja");
+        m.setWarna("hitam");
+        m.setHarga(200.0);
         
         m.mengisiBensin();
     }
