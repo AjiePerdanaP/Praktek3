e4631d777a0b3c291e03a28dbd744d427d76b3f1
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-07 12:41:06 +0700

    buat method mengisibensin(), fix atribut

--- Praktek3/src/praktek3/Motor3.java
+++ Praktek3/src/praktek3/Motor3.java
@@ -3,9 +3,14 @@ package praktek3;
 
 
 public class Motor3 {
-    public static void main(String[] args) {
        String merk;
        String warna;
        Double harga;
+       
+       void mengisiBensin(){
+            System.out.println("Merk \t: "+merk+"\n"+
+                "warna \t: "+warna+"\n"+
+                "harga \t: "+harga);
+    
     }
 }
