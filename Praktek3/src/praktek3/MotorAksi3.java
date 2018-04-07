0169a9624f6913c2f0dd71af365d4a9c7e3d2b27
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-07 12:51:16 +0700

    Instance,buat atribut dan panggil method

--- Praktek3/src/praktek3/MotorAksi3.java
+++ Praktek3/src/praktek3/MotorAksi3.java
@@ -1,5 +1,12 @@
 package praktek3;
 
 public class MotorAksi3 {
-    
+    public static void main(String[] args) {
+        Motor3 m = new Motor3();
+        m.merk="Baja";
+        m.warna="hitam";
+        m.harga=200.0;
+        
+        m.mengisiBensin();
+    }
 }
