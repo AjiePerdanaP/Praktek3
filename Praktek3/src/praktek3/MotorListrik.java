62c802e8e20cc85f13faa6b1bac721d214eb6057
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-07 13:20:48 +0700

    Atribut dan method

--- Praktek3/src/praktek3/MotorListrik.java
+++ Praktek3/src/praktek3/MotorListrik.java
@@ -1,5 +1,9 @@
 package praktek3;
 
 public class MotorListrik extends Motor3 {
-    
+        private String jenismotor;
+        
+        void throttle(){
+            System.out.println("Wiiiiii....");
+        }
 }
